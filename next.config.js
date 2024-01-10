d51f40ae37cc5ae0dd8b421b5a317eab2e5d350c
KyumKyum <mycheesepasta@gmail.com>
2024-01-10 17:01:06 +0900

    🛠️ Refactor: Add remote patterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
+            { // Dev
+                protocol: 'http',
+                hostname: 'localhost:3000',
+            },
+            { // Prod
+                protocol: 'https',
+                hostname: 'watchtower-sigma.vercel.app',
+            }
+        ],
         loader: "default",
         path: "/_next/image",
     },
